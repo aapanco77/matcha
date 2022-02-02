092e4c3085f8775c1c2f70dbfe71d3e273d29023
aapanco77 <alexiapanco@uci.edu.mx>
2022-02-01 18:28:55 -0600

    Commit con javascript nuevo modulo

---
+++ javascript-modulo/inicial.js
@@ -0,0 +1,17 @@
+let number = 0;
+
+let fraccion = 1.5;
+let letter = 'a';
+let chain = 'hola';
+let isTrue = true;
+let isFalse = false;
+
+let n1= 10;
+let n2= 25;
+console.log(chain);
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+a + b
